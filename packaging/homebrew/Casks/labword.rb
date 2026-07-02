17ec9f2589b8bf78d3ccb3ade24cfbfea5b44b88
Felipe Alves <felipe.alves@laborit.com.br>
2026-07-02 15:21:38 -0300

    chore(homebrew): update labword cask to 1.0.3

--- packaging/homebrew/Casks/labword.rb
+++ packaging/homebrew/Casks/labword.rb
@@ -1,8 +1,8 @@
 cask "labword" do
-  version "1.0.2"
-  sha256 "2a7a26d22ea346cfba0fd791d9ad075786406771cc9319e198a558671d49dac1"
+  version "1.0.3"
+  sha256 "d89a22424d89feb733af21fab0fd5b5b2138e26b6ea8c2ac04e5512eb03a05a9"
 
-  url "https://github.com/laboritdev/labword/releases/download/v1.0.2/LabWord-1.0.2-macos-arm64.zip"
+  url "https://github.com/laboritdev/labword/releases/download/v1.0.3/LabWord-1.0.3-macos-arm64.zip"
   name "LabWord"
   desc "Minimalist Markdown editor for macOS by Laborit"
   homepage "https://github.com/laboritdev/labword"
